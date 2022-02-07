1617a8c11bf1e7b53dc4838ff3e9115f26cabc5f
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2022-02-07 17:02:08 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@
     <div class="container">
       <section>
         <div class="flex">
-          <div class="max-w-xs">
+          <div class="max-w-xs mr-10">
             <label for="wallet" class="block text-sm font-medium text-gray-700"
               >Поиск</label
             >
             <div class="mt-1 relative rounded-md shadow-md">
               <input
+                v-model="search"
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="search"
                 type="text"
-                placeholder="Например ipsum ..."
+                placeholder="Например ipsam ..."
               />
             </div>
           </div>
@@ -21,40 +22,97 @@
 
       <hr class="w-full border-t border-gray-600 my-4" />
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        <div
-          v-for="post in posts"
+        <Card
+          class="p-4 bg-white rounded-lg border shadow-md sm:p-6 dark:bg-gray-800 dark:border-gray-700"
+          v-for="post in searchedPosts"
           :key="post.id"
-          class="max-w-sm rounded overflow-hidden shadow-lg bg-white"
-        >
-          <div class="px-6 py-4">
-            <div class="text-xl mb-2">{{ post.id }}</div>
-            <div class="font-bold text-xl mb-2">{{ post.title }}</div>
-            <p class="text-gray-700 text-base">{{ post.body }}</p>
-          </div>
-        </div>
+          :post="post"
+          :form="form"
+          @saveChanges="saveChanges(post, form[post.id])"
+          @disableChanges="disableChanges(post, form[post.id])"
+          @handleDelete="handleDelete(post, form[post.id])"
+        />
       </dl>
     </div>
   </div>
 </template>
 
 <script>
+import Card from "@/components/Card";
 export default {
   name: "App",
+  components: {
+    Card,
+  },
   data() {
     return {
-      posts: {},
+      posts: [],
+      form: [],
+      search: "",
     };
   },
   created() {
+    const postsData = localStorage.getItem("posts");
+
+    if (!postsData) {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((posts) => {
+          this.posts = posts;
+          localStorage.setItem("posts", JSON.stringify(posts));
+        });
+    } else {
+      this.posts = JSON.parse(postsData);
+    }
+
     // Сделать запрос и забрать список постов
-    fetch("http://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((posts) => (this.posts = posts));
 
     // Будем сравнивать ответ сервера и перезаписывать в него изменения пользователя
     // const tickersData = localStorage.getItem("edited-posts");
   },
+  computed: {
+    searchedPosts() {
+      return this.posts.filter((post) => post.title.includes(this.search));
+    },
+  },
   methods: {
+    showButtons() {
+      console.log("123");
+    },
+    disableChanges(newBody) {
+      if (newBody === undefined) {
+        return;
+      }
+
+      const newForm = () => (this.form[Object.keys(this.form)[0]] = "");
+      this.form = newForm;
+    },
+    saveChanges(changedPost, newBody) {
+      if (newBody === undefined) {
+        return;
+      }
+
+      this.posts = this.posts.map((post) => {
+        if (post === changedPost) {
+          const updatedPost = {
+            ...post,
+            body: newBody,
+          };
+
+          post = updatedPost;
+        }
+
+        return post;
+      });
+
+      localStorage.setItem("posts", JSON.stringify(this.posts));
+      const newForm = () => (this.form[Object.keys(this.form)[0]] = "");
+      this.form = newForm;
+    },
+    handleDelete(postToRemove) {
+      this.posts = this.posts.filter((post) => post !== postToRemove);
+      localStorage.setItem("posts", JSON.stringify(this.posts));
+    },
     saveEditedPost() {
       // Нужно для сохранения в локал сторедж
       localStorage.setItem("edited-posts", JSON.stringify(this.tickers));
---
+++ src/components/Card.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <h6 class="mb-3 text-base text-gray-900 lg:text-xl dark:text-white">
+      {{ post.id }}
+    </h6>
+    <h3
+      class="mb-3 text-base font-semibold text-gray-900 lg:text-xl dark:text-white"
+    >
+      {{ post.title }}
+    </h3>
+
+    <p
+      @click="isClicked = true"
+      class="text-sm font-normal cursor-pointer text-gray-500 dark:text-gray-400"
+    >
+      {{ post.body }}
+    </p>
+    <div v-if="isClicked">
+      <div class="max-w-xs">
+        <div class="mt-1 relative rounded-md shadow-md">
+          <input
+            ref="input"
+            v-model="form[post.id]"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="search"
+            type="text"
+            placeholder="Введите новый Body"
+          />
+        </div>
+      </div>
+      <ul class="my-4 space-y-3">
+        <li>
+          <button
+            @click="save()"
+            href="#"
+            class="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+          >
+            <span class="flex-1 ml-3 whitespace-nowrap">Save Changes</span>
+          </button>
+        </li>
+        <li>
+          <button
+            @click="$emit('disableChanges')"
+            href="#"
+            class="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+          >
+            <span class="flex-1 ml-3 whitespace-nowrap">Disable Changes</span>
+          </button>
+        </li>
+        <li>
+          <a
+            @click="$emit('handleDelete')"
+            href="#"
+            class="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+          >
+            <span class="flex-1 ml-3 whitespace-nowrap">Delete Post</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  props: ["post", "form"],
+  data() {
+    return {
+      isClicked: false,
+    };
+  },
+  methods: {
+    save() {
+      if (this.$refs.input.value) {
+        this.$emit("saveChanges");
+
+        this.isClicked = false;
+      }
+    },
+  },
+};
+</script>
